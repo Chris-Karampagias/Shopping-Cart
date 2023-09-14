27de284a2ecee048d6dfa4227ce2953a264908fc
Chris <karampagiaschris@gmail.com>
2023-09-14 20:12:54 +0300

    Add prop validation

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-export default function ErrorPage() {
+function ErrorPage({ error }) {
   return (
     <div className="text-7xl text-center h-[800px] flex justify-center gap-10 flex-col">
       <h1>Whoops! Something went wrong!</h1>
+      <p className="text-gray-500">{error}</p>
       <h2>
         Click{" "}
         <Link className="hover:underline text-orange-500 cursor-pointer" to="/">
@@ -14,3 +16,9 @@ export default function ErrorPage() {
     </div>
   );
 }
+
+ErrorPage.propTypes = {
+  error: PropTypes.oneOfType([(PropTypes.string, PropTypes.object)]),
+};
+
+export default ErrorPage;
