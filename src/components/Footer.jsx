e19358e33d03b34dc49048d14d91fcbb6846d5a8
Chris <karampagiaschris@gmail.com>
2023-09-14 20:11:52 +0300

    Initial implementation of Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+export default function Footer() {
+  return (
+    <div className="h-[100px] text-3xl w-full bg-black flex flex-col gap-2 items-center text-white absolute bottom-0">
+      <h1>Site made by Chris Karampagias</h1>
+      <div className="flex gap-20">
+        <a
+          className="hover:scale-105"
+          href="https://github.com/"
+          target="__blank"
+        >
+          <i className="devicon-github-original-wordmark text-5xl cursor-pointer"></i>
+        </a>
+        <a
+          className="hover:scale-105"
+          href="https://www.linkedin.com/in/chris-karampagias"
+          target="__blank"
+        >
+          <i className="devicon-linkedin-plain text-5xl cursor-pointer"></i>
+        </a>
+      </div>
+    </div>
+  );
+}
