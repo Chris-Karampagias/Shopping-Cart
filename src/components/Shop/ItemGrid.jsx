a0679e5159d265341988aae508402188ca93ea7c
Chris <karampagiaschris@gmail.com>
2023-09-15 19:38:52 +0300

    Pass handleClick function to Item

--- src/components/Shop/ItemGrid.jsx
+++ src/components/Shop/ItemGrid.jsx
@@ -1,7 +1,7 @@
 import Item from "./Item";
 import PropTypes from "prop-types";
 
-function ItemGrid({ products }) {
+function ItemGrid({ products, handleItemClick }) {
   return (
     <section className="min-h-screen w-100% flex justify-center flex-wrap gap-5 p-2 pt-5 pb-40">
       {products.map((product) => {
@@ -11,6 +11,7 @@ function ItemGrid({ products }) {
             title={product.title}
             price={product.price}
             image={product.image}
+            onClick={() => handleItemClick(product)}
           />
         );
       })}
@@ -20,6 +21,7 @@ function ItemGrid({ products }) {
 
 ItemGrid.propTypes = {
   products: PropTypes.array.isRequired,
+  handleItemClick: PropTypes.func.isRequired,
 };
 
 export default ItemGrid;
