d1db56e03c50c4f3b08e4cddf07a01f34523fb32
Chris <karampagiaschris@gmail.com>
2023-09-19 18:28:17 +0300

    Add custom animation to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        showRight: {
+          "0%": { transform: "translateX(800px)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+        showLeft: {
+          "0%": { transform: "translateX(-800px)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+      },
+      animation: {
+        "show-right": "showRight 1s linear forwards ",
+        "show-left": "showLeft 1s linear forwards",
+      },
+    },
   },
   plugins: [],
 };
